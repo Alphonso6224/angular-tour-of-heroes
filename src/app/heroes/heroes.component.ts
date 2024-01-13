af2d3fb5ed2f1003d755b2c41f13ff61e43de8ef
Alphonso6224 <donaldalphonso11@gmail.com>
2024-01-13 12:21:06 +0100

    ajout de service en vue refactoriser le code

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -21,8 +22,20 @@ import { Hero } from '../hero';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
+  constructor(private heroService: HeroService) {}
+
   // comporte le HEROES Tableau pour les liaisons
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
+  // Methode pour récupérer les heros du service
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {

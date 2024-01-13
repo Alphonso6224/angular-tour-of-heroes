205a267e1c3247500ae2a2ff468c5a8ad2902d02
Alphonso6224 <donaldalphonso11@gmail.com>
2024-01-13 11:41:01 +0100

    Affichage d'une liste de héros avec une vue détaillée l'utilisateur peut sélectionner un héros et voir les détails de ce héros. *ngFor(pour afficher les liste), *ngif(incule ou exclure conditionnellement un bloc de code HTML. Utilisation des classes binder

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,29 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Hero } from '../hero';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [
-    CommonModule, FormsModule
+    CommonModule,
+    FormsModule,
+    NgFor,
+    NgIf
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  // Ajout d'une propriété hero
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  // comporte le HEROES Tableau pour les liaisons
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
 }

e718e19be4fbce0ff89a7adb0be3a254e41244f8
Alphonso6224 <donaldalphonso11@gmail.com>
2024-01-13 11:06:21 +0100

    utilisation de données bidirectionnelle avec ngModel grace à l'import de FormsModule

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Hero } from '../hero';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, FormsModule
+  ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
   // Ajout d'une propriété hero
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
